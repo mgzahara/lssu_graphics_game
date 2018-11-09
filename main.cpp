e9de0d3a3d677979d8749ddc2aef86a5b640a22f
Matt Zahara <mattzahara21@gmail.com>
2018-11-09 13:25:53 -0500

    more testing framework keyboard can now change tile type and boost type will help with testing

--- main.cpp
+++ main.cpp
@@ -30,10 +30,10 @@ SDL_Texture *loadImage(const char *);
 int main(int argc, char **args)
 {
   if (!initSDL())
-  {
-    //SDL did not init properly
-    return 1;
-  }
+    {
+      //SDL did not init properly
+      return 1;
+    }
 
   bool done = false;
   int mouse_x = -1, mouse_y = -1;
@@ -42,60 +42,117 @@ int main(int argc, char **args)
   game.startBoard();
 
   while (!done)
-  {
-    //SDL event loop
-    while (SDL_PollEvent(&e) != 0)
     {
-      switch (e.type)
-      {
-      case SDL_QUIT:
-        //close button
-        done = true;
-        break;
+      //SDL event loop
+      while (SDL_PollEvent(&e) != 0)
+	{
+	  switch (e.type)
+	    {
+	    case SDL_QUIT:
+	      //close button
+	      done = true;
+	      break;
 
-      case SDL_MOUSEBUTTONDOWN:
-        if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT))
-        { //grab mouse position on left click
-          SDL_GetMouseState(&mouse_x, &mouse_y);
-        }
-	else if(SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT))
-	  {
-	    //for testing purposes only - remove Tile on right click
-	    SDL_GetMouseState(&mouse_x, &mouse_y);
-	    int clickRow = mouse_y / GRID_SIZE;
-	    int clickCol = mouse_x / GRID_SIZE;
+	    case SDL_MOUSEBUTTONDOWN:
+	      if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT))
+		{ //grab mouse position on left click
+		  SDL_GetMouseState(&mouse_x, &mouse_y);
+		}
+	      else if(SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT))
+		{
+		  //for testing purposes only - remove Tile on right click
+		  SDL_GetMouseState(&mouse_x, &mouse_y);
+		  int clickRow = mouse_y / GRID_SIZE;
+		  int clickCol = mouse_x / GRID_SIZE;
 
-	    if (game.validLeftMouseClick(mouse_x, mouse_y)
-		and board[clickRow][clickCol].isNotBusy())
-	      {
-		board[clickRow][clickCol].changeState("empty");
-	      }
-	  }
-        break;
-      }
-    }
+		  if (game.validLeftMouseClick(mouse_x, mouse_y)
+		      and board[clickRow][clickCol].isNotBusy())
+		    {
+		      board[clickRow][clickCol].changeState("empty");
+		    }
+		}
+	      break;
 
-    //draw board grid - is a grid necessary?
-    game.drawBoard();
 
-    game.handleLeftMouseClick(mouse_x, mouse_y);
 
-    game.updateBoard();
 
-    game.checkForMatches();
+	    case SDL_KEYDOWN:
+	      //testing purposes only - change gem type and boost
+	      if(e.key.keysym.scancode == SDL_SCANCODE_1 or
+		 e.key.keysym.scancode == SDL_SCANCODE_2 or
+		 e.key.keysym.scancode == SDL_SCANCODE_3 or
+		 e.key.keysym.scancode == SDL_SCANCODE_4 or
+		 e.key.keysym.scancode == SDL_SCANCODE_5 or
+		 e.key.keysym.scancode == SDL_SCANCODE_6 or
+		 e.key.keysym.scancode <= SDL_SCANCODE_B or
+		 e.key.keysym.scancode <= SDL_SCANCODE_H or
+		 e.key.keysym.scancode <= SDL_SCANCODE_V or
+		 e.key.keysym.scancode <= SDL_SCANCODE_N or
+		 e.key.keysym.scancode <= SDL_SCANCODE_Z)
+		{
+		  SDL_GetMouseState(&mouse_x, &mouse_y);
+		  int clickRow = mouse_y / GRID_SIZE;
+		  int clickCol = mouse_x / GRID_SIZE;
+		  switch(e.key.keysym.scancode)
+		    {
+		    case SDL_SCANCODE_1:board[clickRow][clickCol].setType(0);
+		      break;
+		    case SDL_SCANCODE_2:board[clickRow][clickCol].setType(1);
+		      break;
+		    case SDL_SCANCODE_3:board[clickRow][clickCol].setType(2);
+		      break;
+		    case SDL_SCANCODE_4:board[clickRow][clickCol].setType(3);
+		      break;
+		    case SDL_SCANCODE_5:board[clickRow][clickCol].setType(4);
+		      break;
+		    case SDL_SCANCODE_6:board[clickRow][clickCol].setType(5);
+		      break;
+		    case SDL_SCANCODE_B:board[clickRow][clickCol].changeBoostMode(1);
+		      break;
+		    case SDL_SCANCODE_H:board[clickRow][clickCol].changeBoostMode(2);
+		      break;
+		    case SDL_SCANCODE_V:board[clickRow][clickCol].changeBoostMode(3);
+		      break;
+		    case SDL_SCANCODE_Z:board[clickRow][clickCol].changeBoostMode(4);
+		      break;
+		    case SDL_SCANCODE_N:board[clickRow][clickCol].changeBoostMode(0);
+		      break;
+		    default:
+		      //do nothing
+		      printf("wrong key\n");
+		    }
 
-    game.drawHighlightBox();
+		  mouse_x = mouse_y = -1;
+		}
+	      break;
 
-    game.drawPanel();
+
+
+
+	    }
+	}
+
+      //draw board grid - is a grid necessary?
+      game.drawBoard();
+
+      game.handleLeftMouseClick(mouse_x, mouse_y);
+
+      game.updateBoard();
+
+      game.checkForMatches();
+
+      game.drawHighlightBox();
+
+      game.drawPanel();
     
-    // Update window
-    SDL_RenderPresent(renderer);
+      // Update window
+      SDL_RenderPresent(renderer);
 
-    //reset mouse position
-    mouse_x = mouse_y = -1;
+      //reset mouse position
+      mouse_x = mouse_y = -1;
 
-    SDL_Delay(20);
-  }
+      SDL_Delay(20);
+    }
 
   return 0;
 }
@@ -113,21 +170,21 @@ bool initSDL()
   //set up everything for SDL
   int x = atexit(quitSDL); //set exit function
   if (x != 0)
-  {
-    fprintf(stderr, "cannot set exit function\n");
-    exit(EXIT_FAILURE);
-  }
+    {
+      fprintf(stderr, "cannot set exit function\n");
+      exit(EXIT_FAILURE);
+    }
 
   //seed rand
   srand(time(NULL));
 
   // See last example for comments
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-  {
-    cout << "Error initializing SDL: " << SDL_GetError() << endl;
-    //system("pause");
-    return false;
-  }
+    {
+      cout << "Error initializing SDL: " << SDL_GetError() << endl;
+      //system("pause");
+      return false;
+    }
   //create window
   window = SDL_CreateWindow("Don't Be Jeweled!",
                             SDL_WINDOWPOS_UNDEFINED,
@@ -137,21 +194,21 @@ bool initSDL()
                             SDL_WINDOW_SHOWN);
 
   if (!window)
-  {
-    cout << "Error creating window: " << SDL_GetError() << endl;
-    //system("pause");
-    return false;
-  }
+    {
+      cout << "Error creating window: " << SDL_GetError() << endl;
+      //system("pause");
+      return false;
+    }
   //create renderer
   renderer = SDL_CreateRenderer(window,
                                 -1,
                                 SDL_RENDERER_ACCELERATED);
 
   if (!renderer)
-  {
-    cout << "Error creating renderer: " << SDL_GetError() << endl;
-    return false;
-  }
+    {
+      cout << "Error creating renderer: " << SDL_GetError() << endl;
+      return false;
+    }
 
   loadTextures();
 
@@ -187,11 +244,11 @@ SDL_Texture *loadImage(const char *filename)
 
   texture = IMG_LoadTexture(renderer, filename);
   if (texture == NULL)
-  {
-    printf("Could not load: %s\n", filename);
-    printf("IMG_LoadTexture Error: %s\n", IMG_GetError());
-    exit(1);
-  }
+    {
+      printf("Could not load: %s\n", filename);
+      printf("IMG_LoadTexture Error: %s\n", IMG_GetError());
+      exit(1);
+    }
 
   return texture;
 }
